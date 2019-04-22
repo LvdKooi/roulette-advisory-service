31a7d37f08c6721d2f4cddebe8ee39c68bc8b6f4
Laurens van der Kooi <lvvdkooi@hotmail.com>
2019-04-22 19:49:21 +0200

    Docker, unittesten, exception handling en alle resources toegevoegd

--- src/main/java/nl/kooi/app/RouletteOutcomeController.java
+++ src/main/java/nl/kooi/app/RouletteOutcomeController.java
@@ -7,6 +7,7 @@ import nl.kooi.representation.RouletteRepresentationObject;
 
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+@RequestMapping(path = "/roulette-outcome")
 @RestController
 public class RouletteOutcomeController {
 
-    @RequestMapping(path="/roulette-outcome/{outcome}")
-    public RouletteRepresentationObject sayOutcome(@PathVariable("outcome") int outcome)  {    // als /roulette-outcome?outcome=.. wordt aangeroepen dan wordt deze methode aangeroepen
+    @RequestMapping(path = "/{outcome}", method = GET, produces = "application/json")
+    public RouletteRepresentationObject sayOutcome(@PathVariable("outcome") int outcome) {
         return new RouletteDomainObject(outcome).toRepresentation();
     }
 
+    @RequestMapping(path = "{outcome}/redblack", method = GET, produces = "application/json")
+    public Outcome sayRedBlack(@PathVariable("outcome") int outcome) {
+        RouletteRepresentationObject representation = new RouletteDomainObject(outcome).toRepresentation();
+        return representation.redBlack;
+    }
+
+    @RequestMapping(path = "{outcome}/oddeven", method = GET, produces = "application/json")
+    public Outcome sayOddEven(@PathVariable("outcome") int outcome) {
+        RouletteRepresentationObject representation = new RouletteDomainObject(outcome).toRepresentation();
+        return representation.oddEven;
+    }
+
+    @RequestMapping(path = "{outcome}/half", method = GET, produces = "application/json")
+    public Outcome sayHalf(@PathVariable("outcome") int outcome) {
+        RouletteRepresentationObject representation = new RouletteDomainObject(outcome).toRepresentation();
+        return representation.half;
+    }
+
+    @RequestMapping(path = "{outcome}/row", method = GET, produces = "application/json")
+    public Outcome sayRow(@PathVariable("outcome") int outcome) {
+        RouletteRepresentationObject representation = new RouletteDomainObject(outcome).toRepresentation();
+        return representation.row;
+    }
+
+    @RequestMapping(path = "{outcome}/dozen", method = GET, produces = "application/json")
+    public Outcome sayDozen(@PathVariable("outcome") int outcome) {
+        RouletteRepresentationObject representation = new RouletteDomainObject(outcome).toRepresentation();
+        return representation.dozen;
+    }
 }
--- src/main/java/nl/kooi/app/exceptions/notValidOutcomeException.java
+++ src/main/java/nl/kooi/app/exceptions/notValidOutcomeException.java
@@ -1,5 +1,9 @@
 package nl.kooi.app.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class notValidOutcomeException extends RuntimeException{
     public notValidOutcomeException(String message){
         super(message);
