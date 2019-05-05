de4c0287ff10ec67a4f534e42f9f9d99a10c4929
Laurens van der Kooi <lvvdkooi@hotmail.com>
2019-05-05 19:50:56 +0200

    OddEven unit test

--- src/main/java/nl/kooi/app/tests/GameDomainObjectTests.java
+++ src/main/java/nl/kooi/app/tests/GameDomainObjectTests.java
@@ -3,11 +3,13 @@ package nl.kooi.app.tests;
 import nl.kooi.app.domain.advises.Game.Game;
 import nl.kooi.app.domain.advises.Game.Roulette.RouletteOneToOne.OddEven;
 import nl.kooi.app.domain.advises.Game.Roulette.RouletteOneToOne.RouletteOneToOne;
+import nl.kooi.representation.advises.OddEvenAdviceRepresentation;
 import org.junit.jupiter.api.Test;
 import nl.kooi.app.domain.RouletteDomainObject;
 import nl.kooi.app.exceptions.notValidOutcomeException;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,22 +23,30 @@ public class GameDomainObjectTests {
 
     @Test
     public void oddEvenTests() {
-        RouletteOneToOne game = new OddEven("5", 'D', 4);
-        ArrayList<Integer> outcomeList = new ArrayList<>();
-        outcomeList.add(1);
-        outcomeList.add(3);
-        outcomeList.add(5);
-        outcomeList.add(7);
-        outcomeList.add(9);
-        outcomeList.add(11);
-        outcomeList.add(13);
+        Game game = new OddEven("5", 'D', 4);
+        OddEvenAdviceRepresentation representation;
+        Integer[] outcomeArray = {1,3,5,7,9,11,13};
+        ArrayList<Integer> outcomeList = new ArrayList();
 
-        for(int singleOutcome : outcomeList) {
+        for(Integer outcome : outcomeArray){
+            outcomeList.add(outcome);
+                    }
+
+        for (int singleOutcome : outcomeList) {
             roulette.setOutcome(singleOutcome);
             game.setHits(roulette);
-                        }
+        }
 
-        game.printAdvice();
+        representation = (OddEvenAdviceRepresentation) game.toRepresentation();
+        assertThat("Advice even", representation.even, is(new BigDecimal(40.00).setScale(2)));
+        assertThat("Advice odd", representation.odd, is(new BigDecimal(0.00).setScale(2)));
+
+        roulette.setOutcome(2);
+        game.setHits(roulette);
+
+        representation = (OddEvenAdviceRepresentation) game.toRepresentation();
+        assertThat("Advice even", representation.even, is(new BigDecimal(5.00).setScale(2)));
+        assertThat("Advice odd", representation.odd, is(new BigDecimal(0.00).setScale(2)));
 
     }
 
