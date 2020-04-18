301345c6ff9c342f21b73fcd901a0ad662c63d82
Laurens van der Kooi <lvvdkooi@hotmail.com>
2020-04-18 12:28:03 +0200

    refactoring

--- src/main/java/nl/kooi/app/domain/rouletteoutcome/RouletteOutcomeHelper.java
+++ src/main/java/nl/kooi/app/domain/rouletteoutcome/RouletteOutcomeHelper.java
@@ -1,13 +1,10 @@
 package nl.kooi.app.domain.rouletteoutcome;
 
-import nl.kooi.app.api.dto.CompoundRouletteOutcomeDto;
+import lombok.experimental.UtilityClass;
 import nl.kooi.app.exceptions.NotValidOutcomeException;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static nl.kooi.app.domain.rouletteoutcome.RouletteOutcome.*;
 
@@ -15,38 +12,18 @@ import static nl.kooi.app.domain.rouletteoutcome.RouletteOutcome.*;
  * @author Laurens van der Kooi
  */
 
-public class CompoundRouletteOutcome {
+@UtilityClass
+public final class RouletteOutcomeHelper {
 
     private static final List<Integer> FIRST_COLUMN_LIST;
     private static final List<Integer> SECOND_COLUMN_LIST;
     private static final List<Integer> THIRD_COLUMN_LIST;
     private static final List<Integer> RED_NUMBERS_LIST;
     private static final List<Integer> BLACK_NUMBERS_LIST;
-    private final Map<RouletteOutcome, Boolean> outcomeBooleanMap;
-    private int outcome;
-
-    public CompoundRouletteOutcome(int outcome) {
-        validateOutcome(outcome);
-        outcomeBooleanMap = new HashMap<>();
-        outcomeBooleanMap.put(BLACK, BLACK.equals(redBlack(outcome)));
-        outcomeBooleanMap.put(RED, RED.equals(redBlack(outcome)));
-        outcomeBooleanMap.put(ODD, ODD.equals(oddEven(outcome)));
-        outcomeBooleanMap.put(EVEN, EVEN.equals(oddEven(outcome)));
-        outcomeBooleanMap.put(FIRST_HALF, FIRST_HALF.equals(half(outcome)));
-        outcomeBooleanMap.put(SECOND_HALF, SECOND_HALF.equals(half(outcome)));
-        outcomeBooleanMap.put(FIRST_COLUMN, FIRST_COLUMN.equals(column(outcome)));
-        outcomeBooleanMap.put(SECOND_COLUMN, SECOND_COLUMN.equals(column(outcome)));
-        outcomeBooleanMap.put(THIRD_COLUMN, THIRD_COLUMN.equals(column(outcome)));
-        outcomeBooleanMap.put(FIRST_DOZEN, FIRST_DOZEN.equals(dozen(outcome)));
-        outcomeBooleanMap.put(SECOND_DOZEN, SECOND_DOZEN.equals(dozen(outcome)));
-        outcomeBooleanMap.put(THIRD_DOZEN, THIRD_DOZEN.equals(dozen(outcome)));
-        outcomeBooleanMap.put(ZERO, isZero(outcome));
-        this.outcome = outcome;
-    }
 
     static {
         FIRST_COLUMN_LIST = new ArrayList<>();
-        SECOND_COLUMN_LIST =new ArrayList<>();
+        SECOND_COLUMN_LIST = new ArrayList<>();
         THIRD_COLUMN_LIST = new ArrayList<>();
         RED_NUMBERS_LIST = new ArrayList<>();
         BLACK_NUMBERS_LIST =new ArrayList<>();
@@ -136,33 +113,6 @@ public class CompoundRouletteOutcome {
 
     private static boolean isZero(int currentInput) {
         return currentInput == 0;
-    }
-
-    public int getOutcome() {
-        return outcome;
-    }
-
-    public Map<RouletteOutcome, Boolean> getOutcomeBooleanMap(){
-        return this.outcomeBooleanMap;
-    }
-
-    @Override
-    public String toString() {
-        return
-                "OUTCOME: " + this.outcome +
-                outcomeBooleanMap;
-    }
-
-    public CompoundRouletteOutcomeDto toRepresentationV1() {
-        CompoundRouletteOutcomeDto representation = new CompoundRouletteOutcomeDto();
-        representation.outcome = this.outcome;
-        representation.outcomeList = outcomeBooleanMap
-                .entrySet()
-                .stream()
-                .filter(Map.Entry::getValue)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-            return representation;
     }
 
 }
