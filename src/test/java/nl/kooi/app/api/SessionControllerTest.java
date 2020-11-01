0439ad30f4bd5f21cb975d1ac9bd669877dd0bfb
Laurens van der Kooi <lvvdkooi@hotmail.com>
2020-11-01 11:29:10 +0100

    More SessionController unittests

--- src/test/java/nl/kooi/app/api/SessionControllerTest.java
+++ src/test/java/nl/kooi/app/api/SessionControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.kooi.app.api.dto.Mapper;
 import nl.kooi.app.api.dto.SessionDto;
 import nl.kooi.app.domain.service.SessionService;
+import nl.kooi.app.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringJUnitConfig(SessionController.class)
@@ -42,34 +44,71 @@ class SessionControllerTest {
 
     @Test
     void createWithUnknownUser() throws Exception {
-        var session = new SessionDto();
-        session.setChipValue(BigDecimal.TEN);
-        session.setUserId(1234);
-
-        var mvcResult = mockMvc.perform(post("/users/1234/sessions/")
-                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(session)))
+        mockMvc.perform(post("/users/1234/sessions/")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(getSessionDto())))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void createWithKnownUser() throws Exception {
-        var session = new SessionDto();
-        session.setChipValue(BigDecimal.TEN);
-        session.setUserId(1234);
 
-        when(sessionService.findByUserId(1234)).thenReturn(List.of(Mapper.map(session)));
-        when(sessionService.save(any())).thenReturn(Mapper.map(session));
+        when(sessionService.findByUserId(1234)).thenReturn(List.of(Mapper.map(getSessionDto())));
+        when(sessionService.save(any())).thenReturn(Mapper.map(getSessionDto()));
 
         var mvcResult = mockMvc.perform(post("/users/1234/sessions/")
-                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(session)))
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(getSessionDto())))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        var response = objectMapper.readValue(mvcResult.getContentAsString(), SessionDto.class);
+
+        assertThat(response.getChipValue()).isEqualTo(getSessionDto().getChipValue());
+        assertThat(response.getUserId()).isEqualTo(getSessionDto().getUserId());
+    }
+
+    @Test
+    void findById() throws Exception {
+        when(sessionService.findByIdAndUserId(1234, 1234)).thenReturn(Mapper.map(getSessionDto()));
+
+        var mvcResult = mockMvc.perform(get("/users/1234/sessions/1234"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        var response = objectMapper.readValue(mvcResult.getContentAsString(), SessionDto.class);
+
+        assertThat(response.getChipValue()).isEqualTo(getSessionDto().getChipValue());
+        assertThat(response.getUserId()).isEqualTo(getSessionDto().getUserId());
+    }
+
+    @Test
+    void findByUnknownId() throws Exception {
+        when(sessionService.findByIdAndUserId(1234, 1234)).thenThrow(new NotFoundException("Unknown"));
+        mockMvc.perform(get("/users/1234/sessions/1234"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteUnknownSession() throws Exception {
+        when(sessionService.findByIdAndUserId(1234, 1234)).thenThrow(new NotFoundException("Unknown"));
+        mockMvc.perform(delete("/users/1234/sessions/1234")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(getSessionDto())))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteKnownSession() throws Exception {
+        when(sessionService.findByIdAndUserId(1234, 1234)).thenReturn(Mapper.map(getSessionDto()));
+        var mvcResult = mockMvc.perform(delete("/users/1234/sessions/1234")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(getSessionDto())))
                 .andExpect(status().isOk());
     }
 
-    @Test
-    void findById() {
-    }
-
-    @Test
-    void delete() {
+    private SessionDto getSessionDto() {
+        var session = new SessionDto();
+        session.setChipValue(BigDecimal.TEN);
+        session.setUserId(1234);
+        return session;
     }
 }
